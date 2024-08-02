cacea91838f30d076af2f752c3ade781117c25c1
rudtj <kyoungseo03@naver.com>
2024-08-02 15:32:28 +0900

    feat: admin 추가

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,24 +1,26 @@
 export const RouterPath = {
-  root: '/',
-  home: '/',
-  category: '/category/:categoryId',
-  myAccount: '/my-account',
-  productsDetail: '/products/:productId',
-  order: '/order',
-  login: '/login',
-  register: '/register',
-  notFound: '*',
+  root: "/",
+  home: "/",
+  category: "/category/:categoryId",
+  myAccount: "/my-account",
+  productsDetail: "/products/:productId",
+  order: "/order",
+  login: "/login",
+  register: "/register",
+  admin: "/admin",
+  notFound: "*",
 };
 
 export const getDynamicPath = {
-  category: (categoryId: string) => RouterPath.category.replace(':categoryId', categoryId),
+  category: (categoryId: string) =>
+    RouterPath.category.replace(":categoryId", categoryId),
   login: (redirect?: string) => {
     const currentRedirect = redirect ?? window.location.href;
     return `${RouterPath.login}?redirect=${encodeURIComponent(currentRedirect)}`;
   },
   productsDetail: (goodsId: number | string) =>
     RouterPath.productsDetail.replace(
-      ':productId',
-      typeof goodsId === 'number' ? goodsId.toString() : goodsId,
+      ":productId",
+      typeof goodsId === "number" ? goodsId.toString() : goodsId
     ),
 };
